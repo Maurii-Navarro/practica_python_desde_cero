9fa9dc97ec9e8ecea6278ddd16e3b75351259f25
Maurii.Navarro <mauriinavarro.contacto@gmail.com>
2025-04-03 03:43:53 -0300

    segundo archivo del dia 3//practica de funciones

---
+++ Dia3.1.py
@@ -0,0 +1,24 @@
+#Aqui sigo practicando funciones en el dia tres solo que aplicando Parametros
+#Sera un archivo del dia tres que seguramente suba el dia cuatro  :)
+
+print("Calculadora version 1.0")
+
+#num1=input(int("Indique el primer valor:  "))
+#num2=input(int("Indique el segundo valor:  "))
+
+def suma(num1,num2):
+    print(num1+num2)
+
+def resta(num1,num2):
+    print(num1-num2)
+
+def mult(num1,num2):
+    print(num1*num2)
+
+def div(num1,num2):
+    print(num1/num2)
+
+suma(5,7)
+resta(12,5)
+mult(999,999)
+div(98,12)
