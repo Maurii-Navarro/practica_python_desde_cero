281998c201006c9437b44386d523f9e3e59c5c64
Maurii.Navarro <mauriinavarro.contacto@gmail.com>
2025-04-02 21:29:56 -0300

    primera practica de funciones sin parametros

---
+++ Dia3.py
@@ -0,0 +1,13 @@
+#Pequeña practica de Funciones en python
+
+def edad(edad):
+    print(f"Yo tengo {edad} años")
+
+edad(22)
+
+
+def mensaje():
+    print("Mensaje impreso desde el llamado de una funcion")
+
+mensaje()
+
