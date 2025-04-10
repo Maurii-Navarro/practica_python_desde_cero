434be1c8edb0b84a55c84cd9d2e14fa15a51ba94
Maurii.Navarro <mauriinavarro.contacto@gmail.com>
2025-04-09 23:13:08 -0300

    Dia 5 hecho, necesito ser mas responsable

---
+++ Dia5.py
@@ -0,0 +1,40 @@
+#No hice ninguna actualizacion durante estos cuatro dias porque estoy trabajndo en una estacion de servicio
+#y los turnos rotativos muchas veces me hacen cansar mas cuando trabajo de noche, durante el dia no quiero hacer nada.
+
+#Continuando con el aprendizaje, hoy estoy viendo el tema de las Funciones  :)
+#haré una funcion simple mostrando una estructura basica de calculos aritmetricos
+
+def suma(valor1,valor2):
+    print(valor1+valor2)
+
+def resta(valor1,valor2):
+    print(valor1-valor2)
+
+def multi(valor1,valor2):
+    print(valor1*valor2)
+
+def div(valor1,valor2):
+    print(valor1/valor2)
+
+def restodiv(valor1,valor2):
+    print(valor1%valor2)
+
+def potencia(valor1,valor2):
+    print(valor1**valor2)
+
+#Aqui llamo a las funciones pasandoles valores mostrando por consola su correcto funcionamiento
+
+suma(22,73)
+resta(69,42)
+multi(8,56)
+div(456,42)
+restodiv(54,3)
+potencia(7,3)
+
+#Se que debo de esforzarme y que estoy descuidando mis estudios sobre todo si quiero trabajar de esto,
+#Pero como consegui hace muy pocas semanas el trabajo me centre en aprender mas que nada a manejarme ahi,
+#y a poder relacionarme mas con esas tareas, sobre todo porque necesito dinero y mas si quiero una buena
+#computadora.  nos vemos mañana si todo sale bien.
+#Maurii
+    
+    
