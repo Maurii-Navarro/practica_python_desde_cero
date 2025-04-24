b6c6984028735fc5d66be1bc2bb9fc488204b8d7
Maurii.Navarro <mauriinavarro.contacto@gmail.com>
2025-04-24 16:13:53 -0300

    pequeño repaso de listas

---
+++ Dia6.py
@@ -0,0 +1,27 @@
+#Despues de varias semanas vuelvo, el trabajar en turnos a la mañana o a la noche
+#es mejor para poder dedicarle unos minutos a subir estas practicas "diarias"
+#mientras pueda durante todos estos dias subire algun archivo de practica/scripts que vaya haciendo poco a poco
+
+#/---------/-----------/---------------/-----------------/-----------------------/-/--------/
+
+#pequeña muestra de practica de listas
+
+frutas=["manzana", "naranjas", "uvas", "bananas"]
+numeros=[12,23,34,45,56,67,78,89,90]
+
+print(numeros[3])
+
+#hay distintos tipos de listas
+listaVacia=[]
+numeros=[1,2,3,4,5,6,7,8,9]
+listaMixta=["pollo", 73, True, "crisis infinita"]
+
+#tambien hay metodos que cumplen distintas funciones
+misNumeros=[99,55,1004,6533,8021,1024,2048,69]
+misNumeros.append(20022)
+len(misNumeros)
+misNumeros.insert(444,3)
+misNumeros.pop([6])
+misNumeros.remove(2)
+misNumeros.clear()
+#los metodos Sort y Reverse son para ordenar o invertir las listas
