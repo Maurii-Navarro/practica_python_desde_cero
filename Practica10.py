f08f33a2253474273526cd31a6ed1c4a83240fe9
Maurii.Navarro <mauriinavarro.contacto@gmail.com>
2025-06-15 02:48:56 -0300

    ya voy a estar actualizando mas seguido mi github

---
+++ Practica10.py
@@ -0,0 +1,13 @@
+#Esto sera otro archivo en donde convierto los valores de las variables de entreo
+# a string o flotantes, (es una practica basica pero para retomar lo que habia empezado desde antes,
+# quiero volver a ver todo pero ser mas constante)
+
+a, b, c = 10, 5.0, "25"
+
+print (str(a))
+print(int(b))
+print(float(c))
+
+print(type(a))
+print(type(b))
+print(type(c))
