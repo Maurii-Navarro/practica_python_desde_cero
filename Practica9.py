4dd59b03b5438c2f2b840168bcc2e087da58b2b9
Maurii.Navarro <mauriinavarro.contacto@gmail.com>
2025-06-15 02:38:00 -0300

    (volvi despues de varios meses, (otra vez))

---
+++ Practica9.py
@@ -0,0 +1,14 @@
+#Despues de desaparecer por varios meses, vuelvo a seguir subiendo
+#Actualizaciones (bastante atrasadas) sobre como voy en mi aprendizaje de python
+
+
+#Practica de METODOS DE CADENAS DE TEXTOS
+
+nombre = "rombicosidodecaedro"
+
+print (len(nombre))
+print(nombre.find("c"))
+print(nombre.capitalize())
+print(nombre.upper())
+print(nombre.lower())
+
