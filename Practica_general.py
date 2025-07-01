cd79bad421484df1fac60e99af58d6640859476e
Maurii.Navarro <mauriinavarro.contacto@gmail.com>
2025-07-01 19:24:18 -0300

    practica de funciones de libreria math

---
+++ Practica_general.py
@@ -0,0 +1,24 @@
+#Practica de Funciones matematicas explicadas
+
+import math
+
+pi= 3.141592
+PI= -3.14
+x,y,z=22,88,73
+
+#funcion round (sirve para redondear un numero)
+print(round(pi))
+# funcion ceil (sirve para redondear para arriba)
+print(math.ceil(pi))
+# funcion floor (redondea para abajo)
+print(math.floor(pi))
+# funcion abs (nos da el valor absoluto de un numero)
+print(abs(PI))
+# funcion pow (es elevar un numero a x potencia)
+print(pow(pi,4))
+# funcion sqrt (sacar la raiz cuadrada de un numero)
+print(math.sqrt(420))
+
+#Funcion que nos dice que numero es el mas grande o el menor
+print(max(x,y,z))
+print(min(x,y,z))
