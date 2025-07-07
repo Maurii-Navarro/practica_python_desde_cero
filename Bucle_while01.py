fb88010be6b88896889fd690c04b02bcf8f82aaa
Maurii.Navarro <mauriinavarro.contacto@gmail.com>
2025-07-07 03:15:50 -0300

    practica de bucle while

---
+++ Bucle_while01.py
@@ -0,0 +1,16 @@
+#Practica de Bucle While 
+
+nombre=""
+
+while len(nombre) == 0:
+    nombre = input("Ingresa tu nombre: ")
+print("Hola! ",nombre)
+
+
+print("Bienvenido al programa!")
+password=print("introduce la contraseña:  ")
+while password != "pollo":
+    password = input("Introduzca la contraseña:  ")
+    if password == "pollo":
+        print("ACCESO CONCEDIDO")
+
