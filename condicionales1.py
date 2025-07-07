329e534599f119e46f5e65f3c7035f53df44a0d9
Maurii.Navarro <mauriinavarro.contacto@gmail.com>
2025-07-07 02:00:15 -0300

    practica de condicionales y operadores logicos

---
+++ condicionales1.py
@@ -0,0 +1,12 @@
+#Practica de condicional + Operadores Logicos
+
+temperatura = int(input("Cual es la temperatura afuera?:  "))
+if temperatura >=0 and temperatura <=30:
+    print("La temperatura esta bien hoy ")
+elif temperatura <0 or temperatura >30:
+    print("Tendras un dia muy jodido")
+
+#El operador "NOT" se utiliza para invertir un resultao de un 
+# condicional de "true" a "false" o viceversa
+
+
